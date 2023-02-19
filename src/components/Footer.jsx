9458509c658d2d392b3864c7b63856367e8f522c
Samuel S. Rodriguez <samusr@gmail.com>
2023-02-19 14:16:58 +0000

    Finish Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,25 @@
+import { IconGithub, IconLinkedin, IconTwitter } from './Icons'
+
 export function Footer () {
   return (
     <footer className='flex flex-col items-center p-4'>
-      <p className='text-base text-dark-gray italic'>Challenge by <a href='https://www.frontendmentor.io?ref=challenge' target='_blank' rel='noreferrer' className='underline'>Frontend Mentor</a></p>
-      <p className='text-base text-dark-gray italic'>Coded by <a href='#'>Your Name Here</a></p>
+      <p className='text-base text-dark-gray italic'>Challenge by <a href='https://www.frontendmentor.io/challenges/room-homepage-BtdBY_ENq' target='_blank' rel='noreferrer' className='underline'>Frontend Mentor</a></p>
+      <p className='flex items-center text-base text-dark-gray italic'>Coded by @ssamusr</p>
+      <div className='flex gap-4'>
+        <a
+          href='https://twitter.com/ssamusr'
+          target='_blank'
+          rel='noreferrer'
+        >
+          <IconTwitter className='w-5 fill-dark-gray hover:fill-[#1DA1F2]' />
+        </a>
+        <a href='https://github.com/ssamusr' target='_blank' rel='noreferrer'>
+          <IconGithub className='w-5  fill-dark-gray hover:fill-[#181717]' />
+        </a>
+        <a href='https://www.linkedin.com/in/samuelsrodriguez/' target='_blank' rel='noreferrer'>
+          <IconLinkedin className='w-5  fill-dark-gray hover:fill-[#0A66C2]' />
+        </a>
+      </div>
     </footer>
   )
 }
